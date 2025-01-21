27b66e3b47331bdd3854378d9b91238f59907dea
felipesora <felipeusora@gmail.com>
2025-01-21 17:52:27 -0300

    Usando useState

--- organo/src/componentes/CampoTexto/index.jsx
+++ organo/src/componentes/CampoTexto/index.jsx
@@ -2,11 +2,21 @@ import "./CampoTexto.css"
 
 const CampoTexto = (props) => {
     
+    // let valor = 'Felipe Sora'
+
+    // const [valor, setValor] = useState('')
+
+    const aoDigitado = (evento) => {
+        // setValor(evento.target.value)
+        // console.log(valor);
+        props.aoAlterado(evento.target.value)
+    }
+
     return (
         <div className="campo-texto">
             <label>{props.label}</label>
 
-            <input type="text" required={props.obrigatorio} placeholder={props.placeholder}/>
+            <input value={props.valor} onChange={aoDigitado} type="text" required={props.obrigatorio} placeholder={props.placeholder}/>
         </div>
     )
 }
