d252f2954a18b091244c3c98557e42176bf3a280
felipesora <felipeusora@gmail.com>
2025-01-22 19:55:38 -0300

    Refatorando código duplicado

--- organo/src/componentes/Formulario/index.jsx
+++ organo/src/componentes/Formulario/index.jsx
@@ -5,15 +5,7 @@ import Botao from '../Botao';
 import { useState } from 'react';
 
 const Formulario = (props) => {
-    const times = [
-        'Programação',
-        'Front-End',
-        'Data Science',
-        'Devops',
-        'UX e Design',
-        'Mobile',
-        'Inovação e Gestão'
-    ]
+
 
     const [nome, setNome] = useState('')
     const [cargo, setCargo] = useState('')
@@ -61,7 +53,7 @@ const Formulario = (props) => {
                 <ListaSuspensa 
                 obrigatorio={true} 
                 label="Time" 
-                itens={times}
+                itens={props.times}
                 valor={time}
                 aoAlterado={time => setTime(time)}
                 />
